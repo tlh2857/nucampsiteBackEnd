396323e5782343e8ad80e9cc38cdfb31b6bf1811
Terry Hillis <49683068+tlh2857@users.noreply.github.com>
2020-02-23 15:58:54 -0600

    Express Session Part 2

--- app.js
+++ app.js
@@ -54,32 +54,18 @@ app.use(session({
 }));
 
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
-  console.log(req.session)
+    console.log(req.session);
+
     if (!req.session.user) {
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-            const err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
-  
-        const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        const user = auth[0];
-        const pass = auth[1];
-        if (user === 'admin' && pass === 'password') {
-         //   res.cookie('user', 'admin', {signed: true});
-         req.session.user = 'admin';  
-         return next(); // authorized
-        } else {
-            const err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
     } else {
-        if (req.session.user === 'admin') {
+        if (req.session.user === 'authenticated') {
             return next();
         } else {
             const err = new Error('You are not authenticated!');
@@ -96,9 +82,6 @@ app.use(express.static(path.join(__dirname, 'public'))); //Express.static - for 
 // just return the image file
 // the public folder is everything that is not processed
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
