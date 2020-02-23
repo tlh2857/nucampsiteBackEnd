9d606bf55904de1aa4aeaba7a42899d6732c4fa3
Terry Hillis <49683068+tlh2857@users.noreply.github.com>
2020-02-23 16:44:33 -0600

    Passport

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var logger = require('morgan');  // logs api requests to console as middleware
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 // require function is retunring another function and then immediately calling this function with session
+const passport = require('passport')
+const authenticate =  require('./authenticate');
 
 
 var indexRouter = require('./routes/index');
@@ -45,6 +47,8 @@ app.use(express.json()); //body parser with Express, says 'parse body as json'
 app.use(express.urlencoded({ extended: false })); // have to decoded from non-URL encoding
 // app.use(cookieParser('12345-67890-09876-54321'));
 
+
+
 app.use(session({
   name:'session-id',
   secret:'12345-67890-09876-54321',
@@ -54,25 +58,24 @@ app.use(session({
 }));
 
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-    if (!req.session.user) {
+    if (!req.user) {
         const err = new Error('You are not authenticated!');
         err.status = 401;
         return next(err);
     } else {
-        if (req.session.user === 'authenticated') {
-            return next();
-        } else {
-            const err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
+       return next();
         }
-    }
+    
 }
 
 app.use(auth);
