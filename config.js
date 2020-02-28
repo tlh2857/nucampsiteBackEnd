a2e57ddc0d345930a7c26f3d75906bb2ff2a756e
Terry Hillis <49683068+tlh2857@users.noreply.github.com>
2020-02-27 20:51:49 -0600

    passport facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '244779749851820', 
+        clientSecret: '9bded7b8a152d37950f346fdd684592d'
+    }
 }
